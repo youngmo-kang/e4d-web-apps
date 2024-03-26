9696c501d722b96f8f8d7b549464c8c131c8172d
Young Mo Kang <ykang@salesforce.com>
2024-03-25 21:12:28 -0400

    TODO: fix CORS

--- report/lib/main.dart
+++ report/lib/main.dart
@@ -1,6 +1,5 @@
 import 'dart:convert';
-import 'dart:io';
-
+import 'package:http/http.dart' as http;
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:csv/csv.dart';
@@ -32,9 +31,11 @@ class _MyWidgetState extends State<MyWidget> {
   // holds user uploaded csv
   List<List<dynamic>> _rowsOfColumns = [];
   // column name that contains the request
-  String _colName = "";
+  final _colNameController = TextEditingController(text: "input");
   // uploaded file name to display to the user
   String _fileName = "not uploaded";
+  final _endpointController =
+      TextEditingController(text: "http://localhost:8080/predictions/blockgen");
 
   bool _isValidCsv(List<List<dynamic>> content) {
     if (content.length < 3) return false;
@@ -66,19 +67,56 @@ class _MyWidgetState extends State<MyWidget> {
     }
   }
 
-  // generate report
-  void _generate() {
-    bool err = _colName.isEmpty ||
-        _rowsOfColumns.isEmpty ||
-        !_rowsOfColumns[0].contains(_colName);
+  Future<List<String>> _postRequest(Uri endpoint, List<String> bodies) async {
+    List<String> responses = [];
+    for (var body in bodies) {
+      var response = await http.post(endpoint,
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: json.encode({"inputPrompt": body}));
+      if (response.statusCode == 200) {
+        responses.add(response.body);
+      } else {
+        throw Exception('Failed to post\n$body');
+      }
+    }
+    return responses;
+  }
 
+  // generate report
+  void _generate() async {
+    final colName = _colNameController.text;
+    bool err = colName.isEmpty ||
+        _rowsOfColumns.isEmpty ||
+        !_rowsOfColumns[0].contains(colName);
     if (err) {
-      _error(
+      return _error(
           'Make sure to upload a csv and indicate which column corresponds to the input requests');
+    }
+
+    final colIdx = _rowsOfColumns[0].indexOf(colName);
+    final List<String> bodies = [];
+    for (var ix = 1; ix < _rowsOfColumns.length; ++ix) {
+      final body = _rowsOfColumns[ix][colIdx];
+      bodies.add(body);
+    }
+
+    try {
+      _postRequest(Uri.parse(_endpointController.text), bodies)
+          .then((responses) {
+        List<List<dynamic>> result = List.from(_rowsOfColumns);
+        result[0].add("output");
+        for (var ix = 1; ix < _rowsOfColumns.length; ++ix) {
+          result[ix].add(responses[ix - 1]);
+        }
+      });
+    } catch (e) {
+      return _error(e.toString());
     }
   }
 
-  _error(String msg) {
+  void _error(String msg) {
     ScaffoldMessenger.of(context).showSnackBar(
       SnackBar(content: Text(msg)),
     );
@@ -113,13 +151,50 @@ class _MyWidgetState extends State<MyWidget> {
             height: 24,
           ),
           SizedBox(
-            height: 40,
-            width: 128,
-            child: TextField(
-              onChanged: (value) => _colName = value,
-              decoration: const InputDecoration(
-                labelText: 'Input column:',
-                border: OutlineInputBorder(),
+            width: 256,
+            child: Form(
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                controller: _colNameController,
+                decoration: const InputDecoration(
+                  labelText: 'Input column',
+                  border: OutlineInputBorder(),
+                ),
+                validator: (value) {
+                  return (value == null ||
+                          value.isEmpty ||
+                          value == "output" ||
+                          value.contains(RegExp(r'\s')))
+                      ? "not a valid column name"
+                      : null;
+                },
+              ),
+            ),
+          ),
+          const SizedBox(
+            height: 24,
+          ),
+          SizedBox(
+            width: 512,
+            child: Form(
+              child: TextFormField(
+                autovalidateMode: AutovalidateMode.always,
+                controller: _endpointController,
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return "not a valid endpoint";
+                  }
+                  try {
+                    Uri.parse(value);
+                  } catch (_) {
+                    return "not a valid endpoint";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Endpoint',
+                  border: OutlineInputBorder(),
+                ),
               ),
             ),
           ),
